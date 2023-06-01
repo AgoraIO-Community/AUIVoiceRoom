d0152c203f6b50476fdb3e9bec40ae00135de60e
朱继超 <984065974@qq.com>
2023-06-01 10:14:56 +0800

    rename download dir name

--- iOS/buildAUIKit.sh
+++ iOS/buildAUIKit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ ! -d "AUIKit" ]; then
   echo "AUIKit not exist!! download it..."
-  mkdir AUIKit
   curl -L -o AUIKit-dev-rename_voicechat_ios.zip https://codeload.github.com/AgoraIO-Community/AUIKit/zip/dev/rename_voicechat_ios
-  unzip AUIKit-dev-rename_voicechat_ios.zip -d AUIKit
+  unzip AUIKit-dev-rename_voicechat_ios.zip
+  mv AUIKit-dev-rename_voicechat_ios AUIKit
   rm AUIKit-dev-rename_voicechat_ios.zip
 fi
