662049225d74303ef65a920fcab1dfcace646a3c
王彤 <1746807718@qq.com>
2023-08-01 15:41:41 +0800

    Add Some Log And Adjusting Partial Structure (#10)
    Code optimization

--- Android/asceneskit/src/main/java/io/agora/asceneskit/voice/VoiceRoomActivity.kt
+++ Android/asceneskit/src/main/java/io/agora/asceneskit/voice/VoiceRoomActivity.kt
@@ -11,6 +11,7 @@ import io.agora.asceneskit.databinding.VoiceRoomActivityBinding
 import io.agora.auikit.model.AUIRoomConfig
 import io.agora.auikit.model.AUIRoomContext
 import io.agora.auikit.model.AUIRoomInfo
+import io.agora.auikit.model.AUIUserThumbnailInfo
 import io.agora.auikit.service.IAUIRoomManager
 import io.agora.auikit.service.http.CommonResp
 import io.agora.auikit.service.http.HttpManager
@@ -19,7 +20,8 @@ import io.agora.auikit.service.http.application.TokenGenerateReq
 import io.agora.auikit.service.http.application.TokenGenerateResp
 import io.agora.auikit.service.rtm.AUIRtmErrorProxyDelegate
 import io.agora.auikit.ui.basic.AUIAlertDialog
-import io.agora.scene.show.utils.PermissionHelp
+import io.agora.auikit.utils.AUILogger
+import io.agora.auikit.utils.PermissionHelp
 import retrofit2.Response
 
 class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
@@ -27,9 +29,10 @@ class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
 
     private val mViewBinding by lazy { VoiceRoomActivityBinding.inflate(LayoutInflater.from(this)) }
     private val mPermissionHelp = PermissionHelp(this)
+    private var service:AUIVoiceRoomService? = null
 
     companion object {
-        private var roomInfo: AUIRoomInfo? = null
+        private var roomInfo: AUIRoomInfo = AUIRoomInfo()
         private var themeId: Int = View.NO_ID
 
         fun launch(context: Context, roomInfo: AUIRoomInfo, themeId: Int = R.style.Theme_VoiceRoom) {
@@ -44,13 +47,29 @@ class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (savedInstanceState != null) {
+            themeId = savedInstanceState.getInt("themeId")
+            roomInfo.roomId = savedInstanceState.getString("roomId").toString()
+            val owner = AUIUserThumbnailInfo()
+            owner.userId = savedInstanceState.getString("userId").toString()
+            owner.userName = savedInstanceState.getString("userName").toString()
+            owner.userAvatar = savedInstanceState.getString("userAvatar").toString()
+            roomInfo.roomOwner = owner
+            roomInfo.onlineUsers = savedInstanceState.getInt("onlineUsers")
+            roomInfo.createTime = savedInstanceState.getLong("createTime")
+            roomInfo.roomName = savedInstanceState.getString("roomName").toString()
+            roomInfo.thumbnail = savedInstanceState.getString("thumbnail").toString()
+            roomInfo.micSeatCount = savedInstanceState.getInt("micSeatCount")
+            roomInfo.password = savedInstanceState.getString("password").toString()
+            roomInfo.micSeatStyle = savedInstanceState.getString("micSeatStyle").toString()
+        }
+
         if (themeId != View.NO_ID) {
             setTheme(themeId)
         }
         setContentView(mViewBinding.root)
         mViewBinding.VoiceRoomView.setFragmentActivity(this)
 
-        val roomInfo = roomInfo ?: return
         mViewBinding.VoiceRoomView.setOnShutDownClick {
             onUserLeaveRoom()
         }
@@ -59,16 +78,23 @@ class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
         }
         mPermissionHelp.checkMicPerm(
             {
-                generateToken(roomInfo.roomId) { config ->
-                    AUIVoiceRoomUikit.launchRoom(
-                        roomInfo,
-                        config,
-                        mViewBinding.VoiceRoomView,
-                        AUIVoiceRoomUikit.RoomEventHandler {
+                roomInfo.let {
+                    generateToken(roomInfo.roomId) { config ->
+                        AUIVoiceRoomUikit.launchRoom(
+                            it,
+                            config,
+                            mViewBinding.VoiceRoomView,
+                            AUIVoiceRoomUikit.RoomEventHandler(
+                                onRoomLaunchSuccess = {
+                                    this.service = it
+                                },
+                                onRoomLaunchFailure = {
 
-                        })
-                    AUIVoiceRoomUikit.subscribeError(roomInfo.roomId, this)
-                    AUIVoiceRoomUikit.bindRespDelegate(this)
+                                }
+                            ))
+                        AUIVoiceRoomUikit.subscribeError(it.roomId, this)
+                        AUIVoiceRoomUikit.bindRespDelegate(this)
+                    }
                 }
             },
             {
@@ -79,7 +105,26 @@ class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        roomInfo.let {
+            outState.putInt("themeId",themeId)
+            outState.putString("roomId", it.roomId)
+            outState.putString("userId", it.roomOwner?.userId)
+            outState.putString("userName", it.roomOwner?.userName)
+            outState.putString("userAvatar", it.roomOwner?.userAvatar)
+            outState.putInt("onlineUsers",it.onlineUsers)
+            outState.putLong("createTime",it.createTime)
+            outState.putString("roomName",it.roomName)
+            outState.putString("thumbnail",it.thumbnail)
+            outState.putInt("micSeatCount",it.micSeatCount)
+            outState.putString("password",it.password)
+            outState.putString("micSeatStyle",it.micSeatStyle)
+        }
+    }
+
     override fun onBackPressed() {
+        AUILogger.logger().d("VoiceRoomActivity", "onBackPressed ...")
         onUserLeaveRoom()
     }
 
@@ -94,6 +139,7 @@ class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
             }
         }
         val userId = AUIRoomContext.shared().currentUserInfo.userId
+        AUILogger.logger().d("VoiceRoomActivity", "generateToken start channelName=${config.channelName} userId=$userId...")
         HttpManager
             .getService(ApplicationInterface::class.java)
             .tokenGenerate(TokenGenerateReq(config.channelName, userId))
@@ -104,16 +150,19 @@ class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
                         config.rtcToken007 = rspObj.rtcToken
                         config.rtmToken007 = rspObj.rtmToken
                         AUIRoomContext.shared()?.commonConfig?.appId = rspObj.appId
+                        AUILogger.logger().d("VoiceRoomActivity", "generateToken update rtcToken/rtmToken...")
                     }
+                    AUILogger.logger().d("VoiceRoomActivity", "generateToken success...")
                     trySuccess.invoke()
                 }
                 override fun onFailure(call: retrofit2.Call<CommonResp<TokenGenerateResp>>, t: Throwable) {
+                    AUILogger.logger().e("VoiceRoomActivity", "generateToken onFailure ${t.message}...")
                     trySuccess.invoke()
                 }
             })
         HttpManager
             .getService(ApplicationInterface::class.java)
-            .tokenGenerate006(TokenGenerateReq(config.rtcChannelName, userId))
+            .tokenGenerate(TokenGenerateReq(config.rtcChannelName, userId))
             .enqueue(object : retrofit2.Callback<CommonResp<TokenGenerateResp>> {
                 override fun onResponse(call: retrofit2.Call<CommonResp<TokenGenerateResp>>, response: Response<CommonResp<TokenGenerateResp>>) {
                     val rspObj = response.body()?.data
@@ -146,13 +195,14 @@ class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
     }
 
     override fun onTokenPrivilegeWillExpire(channelName: String?) {
+        AUILogger.logger().d("VoiceRoomActivity", "onTokenPrivilegeWillExpire $channelName ...")
         generateToken(channelName, onSuccess = {
-            AUIRoomContext.shared().roomConfig = it
+            service?.renew(it)
         })
     }
 
     private fun onUserLeaveRoom() {
-        val owner = (roomInfo?.roomOwner?.userId == AUIRoomContext.shared().currentUserInfo.userId)
+        val owner = (roomInfo.roomOwner?.userId == AUIRoomContext.shared().currentUserInfo.userId)
         AUIAlertDialog(this).apply {
             setTitle("Tip")
             if (owner) {
@@ -172,7 +222,8 @@ class VoiceRoomActivity : AppCompatActivity(), AUIRtmErrorProxyDelegate,
     }
 
     private fun shutDownRoom() {
-        roomInfo?.roomId?.let { roomId ->
+        AUILogger.logger().d("VoiceRoomActivity", "shutDownRoom ...")
+        roomInfo.roomId.let { roomId ->
             AUIVoiceRoomUikit.destroyRoom(roomId)
             AUIVoiceRoomUikit.unsubscribeError(roomId, this@VoiceRoomActivity)
             AUIVoiceRoomUikit.unbindRespDelegate(this@VoiceRoomActivity)
