b11c945b4c37fb7e279fba296451e389641885cc
zhangwei <zw0872@163.com>
2023-10-18 11:04:42 +0800

    [Feat] renew token

--- Android/asceneskit/src/main/java/io/agora/asceneskit/voice/AUIVoiceRoomUikit.kt
+++ Android/asceneskit/src/main/java/io/agora/asceneskit/voice/AUIVoiceRoomUikit.kt
@@ -1,16 +1,25 @@
 package io.agora.asceneskit.voice
 
+import android.util.Log
 import io.agora.auikit.model.AUICreateRoomInfo
 import io.agora.auikit.model.AUIRoomConfig
 import io.agora.auikit.model.AUIRoomContext
 import io.agora.auikit.model.AUIRoomInfo
 import io.agora.auikit.service.IAUIRoomManager
 import io.agora.auikit.service.callback.AUIException
+import io.agora.auikit.service.http.CommonResp
+import io.agora.auikit.service.http.HttpManager
+import io.agora.auikit.service.http.TokenGenerator.generateToken
+import io.agora.auikit.service.http.application.ApplicationInterface
+import io.agora.auikit.service.http.application.TokenGenerateReq
+import io.agora.auikit.service.http.application.TokenGenerateResp
 import io.agora.auikit.service.imp.AUIRoomManagerImplRespResp
 import io.agora.auikit.service.ktv.KTVApi
+import io.agora.auikit.service.rtm.AUIRtmErrorRespObserver
 import io.agora.rtc2.RtcEngine
 import io.agora.rtc2.RtcEngineEx
 import io.agora.rtm2.RtmClient
+import retrofit2.Response
 
 object AUIVoiceRoomUikit {
     private val notInitException =
@@ -23,6 +32,7 @@ object AUIVoiceRoomUikit {
     private var mRtcEngineEx: RtcEngineEx? = null
     private var mKTVApi: KTVApi? = null
     private var mService: AUIVoiceRoomService? = null
+    private val mErrorObservers = mutableListOf<AUIRtmErrorRespObserverImp>()
 
     /**
      * 初始化。
@@ -62,7 +72,15 @@ object AUIVoiceRoomUikit {
         mKTVApi = null
     }
 
-    fun destroyRoom(roomId: String?) {
+    fun destroyRoom(roomId: String) {
+        if (AUIRoomContext.shared().isRoomOwner(roomId)) {
+            mService?.getRoomManager()?.destroyRoom(roomId) {}
+        } else {
+            mService?.getRoomManager()?.exitRoom(roomId) {}
+        }
+        mErrorObservers.filter { it.roomId == roomId }.forEach {
+            mRoomManager?.rtmManager?.proxy?.unRegisterErrorRespObserver(it)
+        }
         mService?.destroyRoom()
         AUIRoomContext.shared().cleanRoom(roomId)
         mService = null
@@ -140,6 +158,10 @@ object AUIVoiceRoomUikit {
 
         mService = roomService
         voiceRoom.bindService(roomService)
+
+        val observer = AUIRtmErrorRespObserverImp(roomInfo.roomId)
+        mErrorObservers.add(observer)
+        mRoomManager?.rtmManager?.proxy?.registerErrorRespObserver(observer)
         eventHandler?.onRoomLaunchSuccess?.invoke(roomService)
     }
 
@@ -154,4 +176,110 @@ object AUIVoiceRoomUikit {
         val onRoomLaunchFailure: ((ErrorCode) -> Unit)? = null,
     )
 
+    private fun generateToken(
+        roomId: String,
+        onSuccess: (AUIRoomConfig) -> Unit,
+        onFailure: (AUIException) -> Unit
+    ) {
+        val config = AUIRoomConfig(roomId)
+        var response = 3
+        var isFailure = false
+        val trySuccess = {
+            response -= 1
+            if (response == 0 && !isFailure) {
+                onSuccess.invoke(config)
+            }
+        }
+        val failure = { ex: AUIException ->
+            if (!isFailure) {
+                isFailure = true
+                onFailure.invoke(ex)
+            }
+        }
+
+        val userId = AUIRoomContext.shared().currentUserInfo.userId
+        HttpManager
+            .getService(ApplicationInterface::class.java)
+            .tokenGenerate(TokenGenerateReq(config.channelName, userId))
+            .enqueue(object : retrofit2.Callback<CommonResp<TokenGenerateResp>> {
+                override fun onResponse(
+                    call: retrofit2.Call<CommonResp<TokenGenerateResp>>,
+                    response: Response<CommonResp<TokenGenerateResp>>
+                ) {
+                    val rspObj = response.body()?.data
+                    if (rspObj != null) {
+                        config.rtcToken = rspObj.rtcToken
+                        config.rtmToken = rspObj.rtmToken
+                        AUIRoomContext.shared().appId = rspObj.appId
+                    }
+                    trySuccess.invoke()
+                }
+
+                override fun onFailure(
+                    call: retrofit2.Call<CommonResp<TokenGenerateResp>>,
+                    t: Throwable
+                ) {
+                    failure.invoke(AUIException(-1, t.message))
+                }
+            })
+        HttpManager
+            .getService(ApplicationInterface::class.java)
+            .tokenGenerate(TokenGenerateReq(config.rtcChannelName, userId))
+            .enqueue(object : retrofit2.Callback<CommonResp<TokenGenerateResp>> {
+                override fun onResponse(
+                    call: retrofit2.Call<CommonResp<TokenGenerateResp>>,
+                    response: Response<CommonResp<TokenGenerateResp>>
+                ) {
+                    val rspObj = response.body()?.data
+                    if (rspObj != null) {
+                        config.rtcRtcToken = rspObj.rtcToken
+                        config.rtcRtmToken = rspObj.rtmToken
+                    }
+                    trySuccess.invoke()
+                }
+
+                override fun onFailure(
+                    call: retrofit2.Call<CommonResp<TokenGenerateResp>>,
+                    t: Throwable
+                ) {
+                    failure.invoke(AUIException(-2, t.message))
+                }
+            })
+        HttpManager
+            .getService(ApplicationInterface::class.java)
+            .tokenGenerate(TokenGenerateReq(config.rtcChorusChannelName, userId))
+            .enqueue(object : retrofit2.Callback<CommonResp<TokenGenerateResp>> {
+                override fun onResponse(
+                    call: retrofit2.Call<CommonResp<TokenGenerateResp>>,
+                    response: Response<CommonResp<TokenGenerateResp>>
+                ) {
+                    val rspObj = response.body()?.data
+                    if (rspObj != null) {
+                        // rtcChorusRtcToken007
+                        config.rtcChorusRtcToken = rspObj.rtcToken
+                    }
+                    trySuccess.invoke()
+                }
+
+                override fun onFailure(
+                    call: retrofit2.Call<CommonResp<TokenGenerateResp>>,
+                    t: Throwable
+                ) {
+                    failure.invoke(AUIException(-3, t.message))
+                }
+            })
+    }
+
+    private class AUIRtmErrorRespObserverImp(val roomId: String) : AUIRtmErrorRespObserver {
+        override fun onTokenPrivilegeWillExpire(channelName: String?) {
+            if (roomId != channelName) {
+                return
+            }
+            generateToken(channelName,
+                { mService?.renew(it) },
+                {
+                    Log.e("AUIVoiceRoomUikit", "onTokenPrivilegeWillExpire >> renew token failed -- $it")
+                })
+        }
+    }
 }
